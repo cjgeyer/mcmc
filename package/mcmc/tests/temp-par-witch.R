7889fb84932c899caf076c0739f860847d0ec70d
Charles J. Geyer <charlie@stat.umn.edu>
2010-06-22 13:45:09 -0500

    This is version 0.8 for CRAN

--- package/mcmc/tests/temp-par-witch.R
+++ package/mcmc/tests/temp-par-witch.R
@@ -3,13 +3,17 @@
 
  set.seed(42)
 
- d <- 9
- witch.which <- c(0.1, 0.3, 0.5, 0.7, 1.0)
+ d <- 3
+ witch.which <- 1 - (1 / 2)^(1 / d) * (1 / 4)^(seq(0, 5) / d)
+ witch.which
+
  ncomp <- length(witch.which)
 
  neighbors <- matrix(FALSE, ncomp, ncomp)
  neighbors[row(neighbors) == col(neighbors) + 1] <- TRUE
  neighbors[row(neighbors) == col(neighbors) - 1] <- TRUE
+ neighbors[row(neighbors) == col(neighbors) + 2] <- TRUE
+ neighbors[row(neighbors) == col(neighbors) - 2] <- TRUE
 
  ludfun <- function(state) {
      stopifnot(is.numeric(state))
@@ -18,15 +22,20 @@
      stopifnot(icomp == as.integer(icomp))
      stopifnot(1 <= icomp && icomp <= ncomp)
      theta <- state[-1]
-     if (any(theta > 1.0)) return(-Inf)
+     if (any(abs(theta) > 1.0)) return(-Inf)
      bnd <- witch.which[icomp]
-     if (any(theta > bnd)) return(0)
-     return(- d * log(bnd))
+     if(bnd >= 1.0)
+         stop(sprintf("witch.which[%d] >= 1.0", icomp))
+     if(bnd <= 0.0)
+         stop(sprintf("witch.which[%d] <= 0.0", icomp))
+     if (all(abs(theta) > bnd))
+         return(- (d + 1) * log(2) - d * log(1 - bnd))
+     return(- (d + 1) * log(2) - log1p(- (1 - bnd)^d))
  }
 
- thetas <- matrix(0.5, ncomp, d)
- out <- temper(ludfun, initial = thetas, neighbors = neighbors, nbatch = 20,
-     blen = 10, nspac = 5, scale = 0.56789, parallel = TRUE, debug = TRUE)
+ thetas <- matrix(0, ncomp, d)
+ out <- temper(ludfun, initial = thetas, neighbors = neighbors, nbatch = 50,
+     blen = 13, nspac = 7, scale = 0.3456789, parallel = TRUE)
 
  names(out)
 
@@ -34,5 +43,28 @@
 
  out$accepti
 
- ### seems to be o. k.
+ ### check that have prob 1 / 2 for corners
+
+ outfun <- function(state) {
+     stopifnot(is.matrix(state))
+     ncomp <- nrow(state)
+     d <- ncol(state)
+     foo <- sweep(abs(state), 1, witch.which)
+     bar <- apply(foo > 0, 1, all) 
+     return(as.numeric(bar))
+ }
+
+ out <- temper(out, outfun = outfun)
+
+ colMeans(out$batch)
+ apply(out$batch, 2, sd) / sqrt(out$nbatch)
+
+ ### try again
+
+ out <- temper(out, blen = 103, outfun = outfun)
+
+ foo <- cbind(colMeans(out$batch),
+     apply(out$batch, 2, sd) / sqrt(out$nbatch))
+ colnames(foo) <- c("means", "MCSE")
+ foo
 
--- package/mcmc/tests/temp-ser-witch.R
+++ package/mcmc/tests/temp-ser-witch.R
@@ -3,36 +3,40 @@
 
  set.seed(42)
 
- d <- 9
- witch.which <- c(0.1, 0.3, 0.5, 0.7, 1.0)
+ d <- 3
+ witch.which <- 1 - (1 / 2)^(1 / d) * (1 / 4)^(seq(0, 5) / d)
+ witch.which
+
  ncomp <- length(witch.which)
 
  neighbors <- matrix(FALSE, ncomp, ncomp)
  neighbors[row(neighbors) == col(neighbors) + 1] <- TRUE
  neighbors[row(neighbors) == col(neighbors) - 1] <- TRUE
+ neighbors[row(neighbors) == col(neighbors) + 2] <- TRUE
+ neighbors[row(neighbors) == col(neighbors) - 2] <- TRUE
 
- ludfun <- function(state, log.pseudo.prior) {
+ ludfun <- function(state) {
      stopifnot(is.numeric(state))
      stopifnot(length(state) == d + 1)
      icomp <- state[1]
      stopifnot(icomp == as.integer(icomp))
      stopifnot(1 <= icomp && icomp <= ncomp)
-     stopifnot(is.numeric(log.pseudo.prior))
-     stopifnot(length(log.pseudo.prior) == ncomp)
      theta <- state[-1]
-     if (any(theta > 1.0)) return(-Inf)
+     if (any(abs(theta) > 1.0)) return(-Inf)
      bnd <- witch.which[icomp]
-     lpp <- log.pseudo.prior[icomp]
-     if (any(theta > bnd)) return(lpp)
-     return(- d * log(bnd) + lpp)
+     if(bnd >= 1.0)
+         stop(sprintf("witch.which[%d] >= 1.0", icomp))
+     if(bnd <= 0.0)
+         stop(sprintf("witch.which[%d] <= 0.0", icomp))
+     if (all(abs(theta) > bnd))
+         return(- (d + 1) * log(2) - d * log(1 - bnd))
+     return(- (d + 1) * log(2) - log1p(- (1 - bnd)^d))
  }
 
- theta.initial <- c(1, rep(0.5, d))
- qux <- c(0, 9.179, 13.73, 16.71, 20.56)
+ initial <- c(1, rep(0, d))
 
- out <- temper(ludfun, initial = theta.initial, neighbors = neighbors,
-     nbatch = 50, blen = 30, nspac = 2, scale = 0.56789,
-     parallel = FALSE, debug = FALSE, log.pseudo.prior = qux)
+ out <- temper(ludfun, initial = initial, neighbors = neighbors,
+     nbatch = 50, blen = 13, nspac = 7, scale = 0.3456789)
 
  names(out)
 
@@ -40,5 +44,59 @@
 
  out$accepti
 
- ### seems to be o. k.
+ colMeans(out$ibatch)
+
+ ### check that have prob 1 / 2 for corners
+
+ outfun <- function(state) {
+     stopifnot(is.numeric(state))
+     icomp <- state[1]
+     stopifnot(icomp == as.integer(icomp))
+     stopifnot(1 <= icomp && icomp <= length(witch.which))
+     theta <- state[-1]
+     foo <- all(abs(theta) > witch.which[icomp])
+     bar <- rep(0, length(witch.which))
+     baz <- rep(0, length(witch.which))
+     bar[icomp] <- as.numeric(foo)
+     baz[icomp] <- 1
+     return(c(bar, baz))
+ }
+
+ out <- temper(out, blen = 103, outfun = outfun, debug = TRUE)
+
+ eta.batch <- out$batch[ , seq(1, ncomp)]
+ noo.batch <- out$batch[ , seq(ncomp + 1, ncomp + ncomp)]
+ eta <- colMeans(eta.batch)
+ noo <- colMeans(noo.batch)
+ mu <- eta / noo
+ eta
+ noo
+ mu
+
+ eta.batch.rel <- sweep(eta.batch, 2, eta, "/")
+ noo.batch.rel <- sweep(noo.batch, 2, noo, "/")
+ mu.batch.rel <- eta.batch.rel - noo.batch.rel
+
+ mu.mcse.rel <- apply(mu.batch.rel, 2, sd) / sqrt(out$nbatch)
+ mu.mcse.rel
+
+ foo <- cbind(mu, mu * mu.mcse.rel)
+ colnames(foo) <- c("means", "MCSE")
+ foo
+
+ ### check decision about within-component or jump/swap
+
+ identical(out$unif.which < 0.5, out$which)
+
+ identical(out$which, out$proposal[ , 1] == out$state[ , 1])
+
+ ### check hastings ratio calculated correctly
+
+ n <- apply(neighbors, 1, sum)
+ i <- out$state[ , 1]
+ istar <- out$proposal[ , 1]
+ foo <- apply(out$state, 1, ludfun)
+ bar <- apply(out$proposal, 1, ludfun)
+ my.log.hastings <- bar - foo - log(n[istar]) + log(n[i])
+ all.equal(my.log.hastings, out$log.hastings)
 
