97cdeed0bd71ba27c6a0c4a4ba6975fedbc6e9a1
Leif Johnson <leif.t.johnson@gmail.com>
2011-12-30 23:16:57 -0800

    Implements all transformations from the paper.

--- package/mcmc/R/morph.R
+++ package/mcmc/R/morph.R
@@ -1,6 +1,7 @@
-isotropic <- function(f) {
+isotropic <- function(f, center) {
   f(1); # force evaluation of f
   function(x) {
+    x <- x + center
     x.norm <- sqrt(sum(x * x))
     if (x.norm == 0)
       rep(0, length(x))
@@ -9,54 +10,110 @@ isotropic <- function(f) {
   }
 }
 
-exponential.superexponetial <- function(p, r) {
+newton.raphson <- function(f, df, x, r) {
+  x0 <- 2 * r
+  while(abs(f(x0) - x) > sqrt(.Machine$double.eps)) {
+    x0 <- x0 - (f(x0) - x) / df(x0)
+  }
+  return(x0)
+}
+
+subexponential <- function(b=1) {
+  if (is.null(b)) b <- 1
+  stopifnot(b > 0)
+  b
+  f.inv <- function(x) ifelse(x > 1/b,
+                              exp(b * x) - exp(1)/3,
+                              (x * b)^3 * exp(1) / 6 + x * b * exp(1) / 2)
+  d.f.inv <- function(x) ifelse(x > 1/b,
+                                n * exp(b * x),
+                                (x * b)^2 * exp(1) / 2 + b * exp(1) / 2)
+  f <- function(x) {
+    if (x > 2 * exp(1) / 3) {
+      log(x + exp(1)/3) / b
+    } else {
+      g <- (sqrt(b^12 * (9*x^2 + exp(2)) - 3 * b^6 * x))^(1/3)
+      exp(1/3) * b / g - g / (exp(1/3) * b^3)
+    }
+  }
+  return(list(f=f, f.inv=f.inv, d.f.inv=d.f.inv))
+}
+
+exponential <- function(r=1, p=3) {
+  if (is.null(p)) p <- 3
+  if (is.null(r)) r <- 1
   stopifnot(p > 2)
   stopifnot(r >= 0)
-  f.inv <- isotropic(function(x) ifelse(x < r, x, x + (x-r)^p))
-  dee.f.inv <- function(x) ifelse(x < r, 1, 1 + p * (x-r)^(p-1))
+  f.inv <- function(x) x + (x-r)^p * (x > r)
+  dee.f.inv <- function(x) 1 + p * (x-r)^(p-1) * (x > r)
   if (p == 3) {
     g <- function(x) {
       n <- sqrt((27*r-27*x)^2 + 108) + 27 * (r - x)
       r + (2/n)^(1/3) - (n/2)^(1/3)/3
     }
-    f <- isotropic(function(x) ifelse(x < r, x, g(x)))
+    f <- function(x) ifelse(x < r, x, g(x))
   } else {
-    # sadly, no general closed form solutions exist.  However, sine p > 2
-    # and R >= 0, x + (x-R)^p is strictly increasing.  A simple binary
-    # search will work.
-    f <- NOTWRITTENYET
+    # No general closed form solution exists.  However, since the transformation
+    # has polynomial form, using the Newton-Raphson method should work well.
+    f <- function(x) ifelse(x < r, x, newton.raphson(f.inv, dee.f.inv, x, r))
   }
-  morph.isotropic(f, f.inv, dee.f.inv)
+  return(list(f=f, f.inv=f.inv, d.f.inv=d.f.inv))
 }
 
-morph.isotropic <- function(f, f.inv, dee.f.inv) {
-  logdetdeeh <- function(x) {
-    x.norm <- sqrt(sum(x * x))
-    if (x.norm == 0)
-      length(x) * log(dee.f.inv(0))
-    else
-      log(dee.f.inv(x.norm)) + (k - 1)*(log(f(x.norm)) - log(x.norm))
+morph <- function(f=NULL, f.inv=NULL, logdetdeeh=NULL,
+                  r=NULL, p=NULL, b=NULL,
+                  center=0) {
+  first.set <- c(is.null(f), is.null(f.inv), is.null(logdetdeeh))
+  second.set <- c(is.null(r), is.null(p), is.null(b))
+  if (!xor(any(first.set), any(second.set))) {
+    stop("Exactly one of the sets of arguments (f, f.inv, logdetdeeh) and (r, p, b) should be non NULL.")
   }
-  morph(isotropic(f), isotropic(f.inv), logdetdeeh)
-}
-
-morph <- function(f, f.inv, logdetdeeh) {
+  if (any(first.set) & !all(first.set)) {
+    stop("f, f.inv and logdetdeeh must all be specified.")
+  }
+  if (any(second.set)) {
+    exp.f <- NULL
+    sub.f <- NULL
+    if (!is.null(r) | !is.null(p)) {
+      exp.f <- exponential(r, p)
+    }
+    if (!is.null(b)) {
+      sub.f <- subexponential(b)
+    }
+    if (!is.null(exp.f) & !is.null(sub.f)) {
+      f <- function(x) isotropic(sub.f$f)(isotropic(exp.f$f)(x))
+      f.inv <-
+        function(x) isotropic(sub.f$f.inv)(isotropic(exp.f$f.inv)(x))
+      logdetdeeh <-
+        function(x)
+          isotropic.logdetdeeh(sub.f$f.inv, sub.f$d.f.inv)(exp.f$f.inv(x)) +
+            isotropic.logdetdeeh(exp.f$f.inv, exp.f$d.f.inv)(x)
+    } else if (!is.null(exp.f)) {
+      f <- isotropic(exp.f$f)
+      f.inv <- isotropic(exp.f$f.inv)
+      logdetdeeh <- isotropic.logdetdeeh(exp.f$f.inv, exp.f$d.f.inv)
+    } else if (!is.null(sub.f)) {
+      f <- isotropic(sub.f$f)
+      f.inv <- isotropic(sub.f$f.inv)
+      logdetdeeh <- isotropic.logdetdeeh(sub.f$f.inv, sub.f$d.f.inv)
+    }
+  }
   # is forcing f, f.inv, logdeeh necessary?
   out <- list()
   out$outfun <- function(outfun) {
     if (is.null(outfun)) {
-      return(function(x, ...) f.inv(x))
+      return(function(x, ...) f.inv(x) + center)
     } else if (is.function(outfun)) {
-      return(function(x, ...) outfun(f.inv(x), ...))
+      return(function(x, ...) outfun(f.inv(x) + center, ...))
     } else {
-      return(function(x, ...) f.inv(x)[outfun])
+      return(function(x, ...) (f.inv(x) + center)[outfun])
     }
   }
 
-  out$transform <- function(x) f(x)
-  out$inverse <- function(x) f.inv(x)
+  out$transform <- function(x) f(x - center)
+  out$inverse <- function(x) f.inv(x) + center
   out$lud <- function(lud) {
-    function(lud, ...) lud(f.inv(x), ...) + logdetdeeh(x)
+    function(lud, ...) lud(f.inv(x) + center, ...) + logdetdeeh(x)
   }
 
   return(out)
