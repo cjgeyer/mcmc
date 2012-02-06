94d99440fea37cf487024513a0bb32ffff0a1fae
Leif Johnson <leif.t.johnson@gmail.com>
2012-02-05 20:11:06 -0800

    Adds morph.metrop.

---
+++ package/mcmc/R/morph.metrop.R
@@ -0,0 +1,55 @@
+morph.metrop <- function(obj, initial, nbatch, blen = 1,
+    nspac = 1, scale = 1, outfun, debug = FALSE, b, r, p, ...)
+UseMethod("morph.metrop")
+
+morph.metrop.morph.metropolis <- function(obj, initial, nbatch, blen = 1,
+    nspac = 1, scale = 1, outfun, debug = FALSE, b, r, p, ...) {
+  if (any(!missing(b), !missing(r), !missing(p))) {
+    obj$morph <- morph(b, r, p)
+  }
+  
+  outfun.save <- ifelse(missing(outfun), obj$outfun, outfun)
+  scale.save <- ifelse(missing(scale), obj$scale, scale)
+  
+  morphed.obj <- metrop.metropolis(morphed.obj,
+                                   nbatch=nbatch,
+                                   blen=blen,
+                                   nspac=nspac,
+                                   scale=morph$scale.fun(scale.save),
+                                   outfun=morph$outfun(outfun.save),
+                                   debug=debug,
+                                   ...)
+  
+  unmorphed.obj <- .morph.unmorph(morphed.obj, morph, outfun.save,
+                                  scale.save)
+  return(unmorphed)
+}
+
+morph.metrop.function <- function(obj, initial, nbatch, blen = 1,
+    nspac = 1, scale = 1, outfun, debug = FALSE, b, r, p, ...) {
+  
+  morph <- morph(b=b, r=r, p=p)
+  
+  outfun.save <- ifelse(missing(outfun), NULL, outfun)
+  scale.save <- scale
+  
+  metrop.out <- metrop.function(morph$lud(obj),
+                                initial=morp$transform(initial),
+                                nbatch=nbatch,
+                                blen=blen,
+                                scale=morph$scale.fun(scale),
+                                outfun=morph$outfun(outfun),
+                                debug=debug,
+                                ...)
+  
+  unmorphed.obj <- .morph.unmorph(metrop.out, morph, outfun.save,
+                                  scale.save)
+  return(unmorphed.obj)
+}
+
+.morph.unmorph <- function(obj, morph, outfun, scale) {
+  obj$final <- morph$inverse(obj$final)
+  obj$outfun <- outfun
+  obj$scale <- scale
+  class(obj) <- c("mcmc", "morph.metropolis")
+}
